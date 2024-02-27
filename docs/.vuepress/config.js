8cf5816a79cfe31c18c7bf74e37edba96380d8c8
XZKJGZS <134248888+XZKJGZS@users.noreply.github.com>
2024-02-27 21:38:35 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
   // theme: 'vdoing', // 使用依赖包主题
   theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
 
-  title: "Orion DeepThinking Lab",
+  title: "OrionAI DeepThinking Lab",
   description: 'Green Sharing Inclusive and Sustainable',
   // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
